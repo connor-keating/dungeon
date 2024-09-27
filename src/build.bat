bc50941ffd72073c05d25852c437407860bbe258
Connor <conkeating32@gmail.com>
2024-09-27 14:29:21 -0400

    Adding first round of code.

---
+++ src/build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set TurnOffWarnings= -wd4189 -wd4201
+set CommonCompilerFlags=-Zi -nologo -W4 %TurnOffWarnings% 
+@REM Add build macros
+set CommonCompilerFlags= -D DEBUG_BUILD=1 %CommonCompilerFlags%
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+
+
+IF NOT EXIST .build mkdir .build
+pushd .build
+
+set files=..\src\main.c
+
+@REM 64-bit build command
+cl %CommonCompilerFlags% %files% -link %CommonLinkerFlags% -OUT:dungeon.exe
+popd
